986b9dafb33b8e887c231a2b80c6f01759ae2d54
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2025-05-01 11:51:26 -0500

    omitted data and scraping scripts

--- scraping/ClassSections/1. subjects/scrape_subjects.py
+++
@@ -1,9 +0,0 @@
-from scraping.utils import download_xml, parse_2_level_xml
-
-fname = 'subjects.xml'
-subjects_master = 'https://courses.illinois.edu/cisapp/explorer/schedule/2025/spring.xml'
-download_xml(subjects_master, fname)
-
-subject_df = parse_2_level_xml(fname, 'subjects', 'subject',
-                               ('abbr', 'href', 'name'), ('id', 'href'))
-subject_df.to_csv('subjects.csv')
